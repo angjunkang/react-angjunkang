bbd0c64164c9096ffc96762c5a498cfcbbad65c1
Jun Kang <irminrics@gmail.com>
2021-12-21 19:17:55 +0800

    Completed site optimization

--- src/components/Content/About/Skills.js
+++ src/components/Content/About/Skills.js
@@ -4,11 +4,40 @@ const Skills = () => {
             <div className="education-text left">
                 <h3 className="section-header">Skills</h3>
                 <div className="education-content">
+                    <div className="section-badge-div">
                         <p className="about-title">General</p>
                         <div class="about-badge-div mt-3">
-                            <h5 className="about-badge">C</h5>
+                            <h5 className="about-badge">HTML/CSS</h5>
                             <h5 className="about-badge">Python</h5>
                             <h5 className="about-badge">Java</h5>
+                            <h5 className="about-badge">JavaScript</h5>
+                            <h5 className="about-badge">JQuery</h5>
+                            <h5 className="about-badge">Swift</h5>
+                            <h5 className="about-badge">Kotlin</h5>
+                            <h5 className="about-badge">C#</h5>
+                            <h5 className="about-badge">C++</h5>
+                        </div>
+                    </div>
+                    <div className="section-badge-div mt-3">
+                        <p className="about-title">Database</p>
+                        <div class="about-badge-div mt-3">
+                            <h5 className="about-badge">SQL Developer</h5>
+                            <h5 className="about-badge">MySQL</h5>
+                            <h5 className="about-badge">Redis</h5>
+                            <h5 className="about-badge">MongoDB</h5>
+                        </div>
+                    </div>
+                    <div className="section-badge-div mt-3">
+                        <p className="about-title">Others</p>
+                        <div class="about-badge-div mt-3">
+                            <h5 className="about-badge">React</h5>
+                            <h5 className="about-badge">NodeJS</h5>
+                            <h5 className="about-badge">Bootstrap</h5>
+                            <h5 className="about-badge">Material Design</h5>
+                            <h5 className="about-badge">ASP.NET</h5>
+                            <h5 className="about-badge">PyQt</h5>
+                            <h5 className="about-badge">JSON</h5>
+                        </div>
                     </div>
                 </div>
             </div>
