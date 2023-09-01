f0f8c1e7546d86f36f6e329300dc538b5cf5d69c
Jun Kang <irminrics@gmail.com>
2023-09-02 00:00:28 +0800

    Update year of study

--- src/components/Content/Introduction/Introduction.js
+++ src/components/Content/Introduction/Introduction.js
@@ -9,7 +9,7 @@ const Introduction = () => {
                     <div className="typeText">
                         <TypeItContent />
                     </div>
-                    <span className="intro-description">I am currently a Year 2 Computer Science Student at National University of Singapore.
+                    <span className="intro-description">I am currently a Final Year Computer Science Student at National University of Singapore.
                     </span>
                     <br />
                     <br />
